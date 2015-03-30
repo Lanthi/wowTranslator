00161933173c26780634b2cb2c0dcbb170df647c
Lanthi <lanthi_adam@hotmail.com>
2015-03-30 14:16:40 +0200

    actulizado

---
+++ Wow Translator 2.0/bin/Release/Obj - 27-3-2015 (1 -- 179832).sql
@@ -0,0 +1,59 @@
+--
+--      WOW Translator 2.0 generated the day 27/03/2015 - 11:38:34
+-- -----------------------------------------------------------------
+
+--       Full database: 179832, missing : 0, added :0      By Lanthi
+
+INSERT INTO `locales_gameobject` VALUES (178325,'','Ouvrir pour accomplir le rite.','Öffnen, um Euren Ritus durchzuführen.','','','Abrir para completar el rito','Abrir para completar el rito','Открыть для проведения ритуала.','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (1623,'','Aciérite sauvage','Wildstahlblume','','','Acérita salvaje','Acérita salvaje','Дикий сталецвет','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (178195,'','Huile chanteguerre','Öl des Kriegshymnenklans','','','Aceite Grito de Guerra','Aceite Grito de Guerra','Масло Песни Войны','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (177785,'','Conteneur de babioles','Schmuckstückbehälter','','','Adorno contenedor','Adorno contenedor','Ящик с блеснами','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175738,'','Aegwynn et la chasse au dragon','Aegwynn und die Drachenjagd','','','Aegwynn y la caza del dragón','Aegwynn y la caza del dragón','Эгвин и Драконья Охота','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175948,'','Cloche d´alerte de Baie-du-Butin','Alarmglocke von Beutebucht','','','Alarma de Bahía del Botín','Alarma de Bahía del Botín','Набатный колокол Пиратской Бухты','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (1728,'','Carpette poussiéreuse','Staubiger Teppich','','','Alfombra polvorienta','Alfombra polvorienta','Пыльный коврик','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (2045,'','Etouffante','Würgetang','','','Alga estranguladora','Alga estranguladora','Удавник','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (19597,'','Cache des druides','Druidenversteck','','','Alijo de druidas','Alijo de druidas','Тайник друидов','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (19596,'','Cache des Dormants','Versteck des Schläfers','','','Alijo de Durmientes','Alijo de Durmientes','Тайник Спящих','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (2704,'','Cache d´explosifs','Sprengstofflager','','','Alijo de explosivos','Alijo de explosivos','Тайный склад взрывчатки','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (113757,'','Cachette d´Ombreforge','Schattenschmiedecache','','','Alijo de Forjatiniebla','Alijo de Forjatiniebla','Тайник Тенегорна','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179501,'','Réserve de Noué Dédodevie','Knot Zwingschraubs Behälter','','','Alijo de Knot Thimblejack','Alijo de Knot Thimblejack','Тайник Уззла Наперстяка','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (58595,'','Planque de la Lame-Ardente','Geheimlager der Brennenden Klinge','','','Alijo del Filo Ardiente','Alijo del Filo Ardiente','Тайник Пылающего Клинка','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179703,'','Cachette du seigneur du Feu','Behälter des Feuerfürsten','','','Alijo del Señor del Fuego','Alijo del Señor del Fuego','Тайник повелителя огня','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (177784,'','Praire géante','Riesenweichpanzermuschel','','','Almeja blanda gigante','Almeja blanda gigante','Гигантский моллюск в мягком панцире','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (2744,'','Palourde géante','Riesenmuschel','','','Almeja gigante','Almeja gigante','Гигантский моллюск','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (177201,'','Grand amiral Daelin Portvaillant','Großadmiral Daelin Prachtmeer','','','Almirante general Daelin Valiente','Almirante general Daelin Valiente','Великий адмирал Даэлин Праудмур','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179828,'','Oreiller sombrefer','Dunkeleisenkissen','','','Almohada Hierro Negro','Almohada Hierro Negro','Подушка дворфа Черного Железа','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179832,'','Oreiller brodé de Pincetaie','Verziertes Kissen von Kissenquetscher','','','Almohada ornamentada de Pilaprieta','Almohada ornamentada de Pilaprieta','Вышитая подушка Крепких Тисков','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (148836,'','Autel d´Hakkar','Altar von Hakkar','','','Altar de Hakkar','Altar de Hakkar','Алтарь Хаккара','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (2576,'','Autel des marées','Altar der Gezeiten','','','Altar de las Mareas','Altar de las Mareas','Алтарь Приливов','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (153205,'','Autel du Souilleur','Altar des Entweihers','','','Altar de los Rapiñadores','Altar de los Rapiñadores','Алтарь Осквернителя','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (148498,'','Autel de Suntara','Altar von Suntara','','','Altar de Suntara','Altar de Suntara','Алтарь Сунтары','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (149420,'','Ani','Ani','','','Ani','Ani','Ани','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175763,'','Anciennes haines - La colonisation de Kalimdor','Alter Hass - Die Kolonialisierung von Kalimdor','','','Antiguas rencillas: la colonización de Kalimdor','Antiguas rencillas: la colonización de Kalimdor','Древняя ненависть – Колонизация Калимдора','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (149047,'','Torche de vindicte','Fackel der Vergeltung','','','Antorcha de reprensión','Antorcha de reprensión','Факел воздаяния','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (22234,'','Torche d´avertissement','Signalfackel','','','Antorcha de señalización','Antorcha de señalización','Сигнальный факел','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (176753,'','Herbe maléfique','Verdammniskraut','','','Apocalíseas','Apocalíseas','Погибельник','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175659,'','Ci-gît le roi Terenas Menethil II','Hier ruht König Terenas Menethil II.','','','Aquí yace el rey Terenas Menethil II','Aquí yace el rey Terenas Menethil II','Здесь покоится Теренас Менетил II','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175734,'','Arathor et les Guerres trolles','Arathor und die Trollkriege','','','Arathor y las Guerras Trols','Arathor y las Guerras Trols','Аратор и Тролльские Войны','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (19016,'','Buisson couvert de poussière d´étoile','Mit Sternenstaub bedeckter Busch','','','Arbusto cubierto de polvo estelar','Arbusto cubierto de polvo estelar','Кустарник, покрытый звездной пылью','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175382,'','Coffret de Frèteruine','Doomriggers Kasten','','','Arca de Equipasino','Arca de Equipasino','Металлический ящик Рокового Зажима','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (160836,'','Coffre aux reliques','Reliktkasten','','','Arca de reliquias','Arca de reliquias','Реликварий','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (160845,'','Coffre sombre','Dunkler Kasten','','','Arca oscura','Arca oscura','Черный ящик','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (178244,'','Coffret d´entraînement','Übungsschließkassette','','','Arcón de prácticas','Arcón de prácticas','Учебный сейф','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (2718,'','Coffret de Trelane','Trelanes Schließkassette','','','Arcón de Trelane','Arcón de Trelane','Сейф Трелана','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175165,'','Coffret de l´Aube argentée','Schließkassette der Silbermorgen','','','Arcón del Amanecer de Plata','Arcón del Amanecer de Plata','Сейф Серебряной Зари','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175166,'','Coffret du Voile de brume','Schließkassette der Nebelschleier','','','Arcón del Halo de bruma','Arcón del Halo de bruma','Сейф Туманной Пелены','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (141980,'','Coffret spectral','Geisterhafte Schließkassette','','','Arcón espectral','Arcón espectral','Призрачный сейф','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (177790,'','Etrange coffret','Merkwürdige Schließkassette','','','Arcón extraño','Arcón extraño','Необычный сейф','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175802,'','Petit coffret','Kleine Schließkassette','','','Arcón pequeño','Arcón pequeño','Маленький сейф','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (25333,'','Archimage Khadgar du Kirin Tor','Erzmagier Khadgar von den Kirin Tor','','','Archimago Khadgar del Kirin Tor','Archimago Khadgar del Kirin Tor','Верховный маг Кадгар из Кирин-Тора','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (176245,'','Archives écarlates','Scharlachrotes Archiv','','','Archivo Escarlata','Archivo Escarlata','Архивы Алого ордена','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175924,'','Cabinet verrouillé','Verschlossener Schrank','','','Armario cerrado','Armario cerrado','Запертый шкаф','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (19877,'','Placard de Velinde','Velindes Spind','','','Armario de Velinde','Armario de Velinde','Сундучок Велинды','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (41,'','Placard de l´ambassadeur','Spind des Botschafters','','','Armario del Embajador','Armario del Embajador','Сундук посла','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (176944,'','Vieux coffre au trésor','Alte Schatztruhe','','','Arqueta antigua','Arqueta antigua','Старый сундук с сокровищами','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179697,'','Coffre au trésor de l´arène','Arenaschatztruhe','','','Arqueta de la arena','Arqueta de la arena','Сундук с сокровищами арены','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (30854,'','Artéfact atal´ai','Artefakt der Atal´ai','','','Artefacto Atal´ai','Artefacto Atal´ai','Артефакт Атал´ай','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (148506,'','Artéfact du Crépuscule','Schattenhammerartefakt','','','Artefacto Crepuscular','Artefacto Crepuscular','Сумеречный Артефакт','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (177199,'','Archevêque Alonsus Faol','Erzbischof Alonsus Faol','','','Arzobispo Alonsus Faol','Arzobispo Alonsus Faol','Архиепископ Алонсус Фаол','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (175686,'','Astrolabe des Bien-nés','Astrolabium der Hochgeborenen','','','Astrolabio Altonato','Astrolabio Altonato','Астролябия высокорожденных','','','','','','','','','18019');
