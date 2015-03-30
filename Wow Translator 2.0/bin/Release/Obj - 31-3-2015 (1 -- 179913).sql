c5bc4be29a02fa976673994257efe258a8a51db7
Lanthi <lanthi_adam@hotmail.com>
2015-03-31 01:28:01 +0200

    actu

---
+++ Wow Translator 2.0/bin/Release/Obj - 31-3-2015 (1 -- 179913).sql
@@ -0,0 +1,58 @@
+--
+--      WOW Translator 2.0 generated the day 31/03/2015 - 0:16:38
+-- -----------------------------------------------------------------
+
+--       Full database: 179913, missing : 0, added :0      By Lanthi
+
+INSERT INTO `locales_gameobject` VALUES (177927,'','Petit plant de Gaïa de Tammra','Tammras Gaeaschössling','','','Árbol joven de Gaia tammra','Árbol joven de Gaia tammra','Саженец Геи Таммры','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (148838,'','Idole d´Hakkar','Götze von Hakkar','','','Ídolo de Hakkar','Ídolo de Hakkar','Идол Хаккара','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179913,'','Aux armes !','Zu den Waffen!','','','¡A las armas!','¡A las armas!','К оружию!','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (1726,'','Disparu !','Vermisst:','','','¡Desaparecido!','¡Desaparecido!','Пропали без вести!','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (2008,'','Dangereux !','Gefährlich!','','','¡Peligro!','¡Peligro!','ОПАСНО!','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (47,'','Avis de recherche !','GESUCHT:','','','¡Se busca!','¡Se busca!','Розыск!','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (256,'','Avis de recherche !','GESUCHT','','','¡Se busca!','¡Se busca!','Разыскивается!','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (711,'','Avis de recherche !','GESUCHT!','','','¡Se busca!','¡Se busca!','Розыск!','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (142194,'','Trésor de pirates !','Piratenschatz!','','','¡Tesoro de pirata!','¡Tesoro de pirata!','Пиратское сокровище!','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (2087,'','Ordres de la Voile sanglante','Blutsegelbefehle','','','Órdenes de los Velasangre','Órdenes de los Velasangre','Приказы пиратов Кровавого Паруса','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179492,'','Cantine abîmée','Verbeulte Schließkiste','','','Baúl abollado','Baúl abollado','Проломленный сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179494,'','Cantine abîmée','Verbeulte Schließkiste','','','Baúl abollado','Baúl abollado','Проломленный сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179496,'','Cantine abîmée','Verbeulte Schließkiste','','','Baúl abollado','Baúl abollado','Проломленный сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (3643,'','Vieille cantine','Alte Schließkiste','','','Baúl antiguo','Baúl antiguo','Старый сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (20691,'','Cantine de Cozzle','Cozzles Schließkiste','','','Baúl de Cozzle','Baúl de Cozzle','Сундучок Коззла','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (2717,'','Cantine de Trelane','Trelanes Schließkiste','','','Baúl de Trelane','Baúl de Trelane','Сундучок Трелана','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (35,'','Coffre du capitaine','Schließkiste des Kapitäns','','','Baúl del Capitán','Baúl del Capitán','Сундучок капитана','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179487,'','Cantine détrempée','Durchnässte Schließkiste','','','Baúl encharcado','Baúl encharcado','Затопленный сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179491,'','Cantine détrempée','Durchnässte Schließkiste','','','Baúl encharcado','Baúl encharcado','Затопленный сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179498,'','Cantine écarlate','Scharlachrote Schließkiste','','','Baúl Escarlata','Baúl Escarlata','Сундучок Алого ордена','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179486,'','Cantine endommagée','Ramponierte Schließkiste','','','Baúl maltrecho','Baúl maltrecho','Побитый сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179488,'','Cantine endommagée','Ramponierte Schließkiste','','','Baúl maltrecho','Baúl maltrecho','Побитый сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179493,'','Cantine moisie','Moosbedeckte Schließkiste','','','Baúl mohoso','Baúl mohoso','Замшелый сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (179497,'','Cantine moisie','Moosbedeckte Schließkiste','','','Baúl mohoso','Baúl mohoso','Замшелый сундучок','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (1593,'','Bateau chargé de cadavres','Boot mit Leichen','','','Barco cargado de cadáveres','Barco cargado de cadáveres','Корабль с трупами','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (3658,'','Tonneau d´eau','Wasserfass','','','Barrica de agua','Barrica de agua','Бочка с водой','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (269,'','Tonneau de Tonneblonde gardé','Bewachtes Fass mit Donnerbräu Lager','','','Barrica de Cerveza del Trueno custodiado','Barrica de Cerveza del Trueno custodiado','Охраняемая бочка Громового эля','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (270,'','Tonneau de Tonneblonde non gardé','Unbewachtes Fass mit Donnerbräu Lager','','','Barrica de Cerveza del Trueno sin vigilar','Barrica de Cerveza del Trueno sin vigilar','Неохраняемая бочка Громового эля','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (3705,'','Tonneau de lait','Fass mit Milch','','','Barrica de leche','Barrica de leche','Бочка молока','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (3706,'','Tonneau de nectar sucré','Fass mit süßem Nektar','','','Barrica de néctar dulce','Barrica de néctar dulce','Бочка сладкого нектара','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (177491,'','Tonneau de termites','Termitenfass','','','Barrica de termitas','Barrica de termitas','Термитная бочка','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (3659,'','Tonneau de jus de melon','Fass Melonensaft','','','Barrica de zumo de melón','Barrica de zumo de melón','Бочка дынного сока','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (36,'','Tonneau cassé','Zerbrochenes Fass','','','Barrica rota','Barrica rota','Разбитая бочка','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (142151,'','Tonneau scellé','Versiegeltes Fass','','','Barrica sellada','Barrica sellada','Запечатанная бочка','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (259,'','Tonneau à moitié enseveli','Halb vergrabenes Fass','','','Barrica semienterrada','Barrica semienterrada','Полузакопанная бочка','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (257,'','Tonneau suspect','Verdächtiges Fass','','','Barrica sospechosa','Barrica sospechosa','Подозрительная бочка','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (20737,'','Tonneau camouflé avec zèle','Geschickt verkleidetes Fass','','','Barrica visiblemente camuflada','Barrica visiblemente camuflada','Тщательно замаскированная бочка','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (164911,'','Tonneau de Tonnebière','Fässchen mit Donnerbräu Lagerbier','','','Barril de cerveza Cebatruenos','Barril de cerveza Cebatruenos','Бочонок громоварского светлого','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (1727,'','Tonneau de Croquejarret brune','Fässchen Kniegräberstarkbier','','','Barril de cerveza Machacacanillas','Barril de cerveza Machacacanillas','Бочонок &quot;Землекопского портера&quot;','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (178145,'','Gangue de résonite','Resonitbehälter','','','Barril de resonita','Barril de resonita','Резонитовый ящик','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (19904,'','Tabac de Mok´Morokk','Mok´Morokks Schnupftabak','','','Barril de tabaco de Mok'Morokk','Barril de tabaco de Mok'Morokk','Понюшка Мок´Морокка','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (3238,'','Tonneau vide de Chen','Chens leeres Fässchen','','','Barril vacío de Chen','Barril vacío de Chen','Пустой бочонок Чена','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (154357,'','Boue luisante','Glitzernder Matsch','','','Barro centelleante','Barro centelleante','Блестящая грязь','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (176317,'','Le Coffre d´argent','Der Argentumtresor','','','Bastión Argenta','Bastión Argenta','Серебряный Оплот','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (19595,'','Banque du portier','Unterstand des Torwächters','','','Bastión de los Porteros','Bastión de los Porteros','Светильник Привратника','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (2912,'','Pomme de pin','Bernsteinkorn','','','Bellota ámbar','Bellota ámbar','Янтарное семя','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (1673,'','Gangrenoix','Teufelszapfen','','','Bellota vil','Bellota vil','Шишка Скверны','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (164888,'','Navetille corrompue','Verderbte Peitscherwurzel','','','Blancoria corrupta','Blancoria corrupta','Гнилой кнутокорень','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (20807,'','Brasero antique','Uralte Kohlenpfanne','','','Blandón antiguo','Blandón antiguo','Древняя жаровня','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (149025,'','Brasero de sentinelle','Wachpostenkohlenpfanne','','','Blandón de avizor','Blandón de avizor','Жаровня на башне','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (152097,'','Brasero de Belnistrasz','Belnistrasz´ Kohlenpfanne','','','Blandón de Belnistrasz','Blandón de Belnistrasz','Жаровня Белнистраза','','','','','','','','','18019');
+INSERT INTO `locales_gameobject` VALUES (61934,'','Brasero de la Flamme Dormante','Kohlenpfanne der schlafenden Flamme','','','Blandón de la Llama Latente','Blandón de la Llama Latente','Жаровня Дремлющего Пламени','','','','','','','','','18019');
